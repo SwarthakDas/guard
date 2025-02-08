ae40b347f25e2b372153d8c1ff7c504576a9d1f8
SwarthakDas <swarthakd@gmail.com>
2025-02-08 23:28:34 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import helmet from "helmet"
+import morgan from "morgan"
+import cors from "cors"
+import express from "express"
+import mongoose from "mongoose"
+
+dotenv.config()
+const app=express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors())
+
+const PORT=process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
+})
+.catch((error)=>console.error(`${error} did not connect`))
