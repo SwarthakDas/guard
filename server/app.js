51dfe1707ee3af2518a360c51430baf35aac3431
SwarthakDas <swarthakd@gmail.com>
2025-06-22 23:39:35 +0530

    Increase JSON and URL-encoded body size limits to 30mb

--- server/app.js
+++ server/app.js
@@ -14,8 +14,8 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit:"1mb"}))
-app.use(express.urlencoded({extended:true, limit:"1mb"}))
+app.use(express.json({limit:"30mb"}))
+app.use(express.urlencoded({extended:true, limit:"30mb"}))
 app.use(cookieParser())
 
 app.use("/auth",authRoutes)
